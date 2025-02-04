5946d3242a6ecd64b5448317a31a26710f0d3bb1
Gili "OpenBagTwo" Barlev <openbagtwo@gmail.com>
2025-02-04 13:27:57 -0500

    Add files from Fabric template

---
+++ src/client/java/io/github/openbagtwo/lighterend/LighterEndClient.java
@@ -0,0 +1,10 @@
+package io.github.openbagtwo.lighterend;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LighterEndClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/java/io/github/openbagtwo/lighterend/LighterEndDataGenerator.java
@@ -0,0 +1,11 @@
+package io.github.openbagtwo.lighterend;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class LighterEndDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
