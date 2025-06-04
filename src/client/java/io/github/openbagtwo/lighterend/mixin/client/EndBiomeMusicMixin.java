80fa68af880ec71bdda1ff7d2629fe7d76a951c4
Gili "OpenBagTwo" Barlev <openbagtwo@gmail.com>
2025-06-04 19:10:51 -0400

    Add biome sounds
    TIL that end music is hard-coded in Vanilla!

---
+++ src/client/java/io/github/openbagtwo/lighterend/mixin/client/EndBiomeMusicMixin.java
@@ -0,0 +1,60 @@
+package io.github.openbagtwo.lighterend.mixin.client;
+
+import java.util.Optional;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.hud.InGameHud;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.client.sound.MusicInstance;
+import net.minecraft.registry.entry.RegistryEntry;
+import net.minecraft.sound.MusicSound;
+import net.minecraft.util.Nullables;
+import net.minecraft.util.collection.Pool;
+import net.minecraft.world.World;
+import net.minecraft.world.biome.Biome;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(MinecraftClient.class)
+public abstract class EndBiomeMusicMixin {
+
+  @Shadow
+  public @Nullable Screen currentScreen;
+
+  @Shadow
+  public @Nullable ClientPlayerEntity player;
+
+  @Shadow
+  public InGameHud inGameHud;
+
+
+  @Inject(method = "getMusicInstance", at = @At("HEAD"), cancellable = true)
+  public void checkForEndMusic(CallbackInfoReturnable<MusicInstance> cir) {
+    MusicSound musicSound = Nullables.map(this.currentScreen, Screen::getMusic);
+    if (musicSound == null && this.player != null) {
+      World world = this.player.getWorld();
+      if (
+          world.getRegistryKey() == World.END
+              && !this.inGameHud.getBossBarHud().shouldPlayDragonMusic()
+      ) {
+        RegistryEntry<Biome> registryEntry = world.getBiome(this.player.getBlockPos());
+        Biome biome = registryEntry.value();
+        Optional<Pool<MusicSound>> biomeMusic = biome.getMusic();
+        if (biomeMusic.isPresent()) {
+          float f = biome.getMusicVolume();
+          Optional<MusicSound> music = biomeMusic.get().getOrEmpty(world.random);
+          if (music.isPresent()) {
+            cir.setReturnValue(new MusicInstance(music.get(), f));
+            cir.cancel();
+          }
+        }
+      }
+    }
+
+  }
+
+}
