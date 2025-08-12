be12173414be5c0347bd64e2d2dbdc87cf884719
Gili "OpenBagTwo" Barlev <openbagtwo@gmail.com>
2025-08-12 14:41:02 -0400

    Painted crab

--- src/client/java/io/github/openbagtwo/lighterend/mobs/models/CrabModel.java
+++ src/client/java/io/github/openbagtwo/lighterend/mobs/models/CrabModel.java
@@ -22,6 +22,8 @@ public class CrabModel extends EntityModel<LivingEntityRenderState> {
   private final ModelPart lefthind;
   private final ModelPart leftfore;
   private final ModelPart rightfore;
+  private final ModelPart leftpincer;
+  private final ModelPart rightpincer;
 
   public CrabModel(ModelPart root) {
     super(root);
@@ -30,41 +32,49 @@ public class CrabModel extends EntityModel<LivingEntityRenderState> {
     this.lefthind = this.body.getChild("lefthind");
     this.leftfore = this.body.getChild("leftfore");
     this.rightfore = this.body.getChild("rightfore");
+    this.leftpincer = this.body.getChild("leftpincer");
+    this.rightpincer = this.body.getChild("rightpincer");
   }
 
   public static TexturedModelData getTexturedModelData() {
     ModelData modelData = new ModelData();
     ModelPartData modelPartData = modelData.getRoot();
     ModelPartData body = modelPartData.addChild("body", ModelPartBuilder.create().uv(0, 0)
-            .cuboid(-8.0F, -11.0F, 4.0F, 24.0F, 8.0F, 16.0F, new Dilation(0.0F))
-            .uv(48, 31).cuboid(-4.0F, -11.0F, 20.0F, 16.0F, 8.0F, 8.0F, new Dilation(0.0F))
-            .uv(0, 24).cuboid(-8.0F, -7.0F, -12.0F, 8.0F, 8.0F, 16.0F, new Dilation(0.0F))
-            .uv(0, 48).cuboid(8.0F, -7.0F, -12.0F, 8.0F, 8.0F, 16.0F, new Dilation(0.0F)),
+            .cuboid(-8.0F, -11.9F, 4.0F, 24.0F, 9.0F, 16.0F, new Dilation(0.0F))
+            .uv(0, 59).cuboid(-4.0F, -11.0F, 20.0F, 16.0F, 8.0F, 8.0F, new Dilation(0.0F)),
         ModelTransform.origin(-4.0F, 23.0F, -10.0F));
 
-    ModelPartData face_r1 = body.addChild("face_r1", ModelPartBuilder.create().uv(48, 24)
+    ModelPartData face_r1 = body.addChild("face_r1", ModelPartBuilder.create().uv(0, 51)
             .cuboid(-12.0F, 0.0F, 0.0F, 24.0F, 0.0F, 7.0F, new Dilation(0.0F)),
         ModelTransform.of(4.0F, -11.0F, 5.0F, 2.1817F, 0.0F, 0.0F));
 
-    ModelPartData righthind = body.addChild("righthind", ModelPartBuilder.create().uv(48, 63)
+    ModelPartData righthind = body.addChild("righthind", ModelPartBuilder.create().uv(0, 76)
             .cuboid(-12.0F, -2.0F, -2.1F, 14.0F, 4.0F, 4.0F, new Dilation(0.0F))
-            .uv(32, 72).cuboid(-12.0F, 2.0F, -2.1F, 4.0F, 4.0F, 4.0F, new Dilation(0.0F)),
+            .uv(81, 9).cuboid(-12.0F, 2.0F, -2.1F, 4.0F, 4.0F, 4.0F, new Dilation(0.0F)),
         ModelTransform.origin(-8.0F, -5.0F, 18.0F));
 
-    ModelPartData lefthind = body.addChild("lefthind", ModelPartBuilder.create().uv(48, 71)
+    ModelPartData lefthind = body.addChild("lefthind", ModelPartBuilder.create().uv(37, 77)
             .cuboid(-2.0F, -2.0F, -2.1F, 14.0F, 4.0F, 4.0F, new Dilation(0.0F))
-            .uv(48, 79).cuboid(8.0F, 2.0F, -2.1F, 4.0F, 4.0F, 4.0F, new Dilation(0.0F)),
+            .uv(0, 85).cuboid(8.0F, 2.0F, -2.1F, 4.0F, 4.0F, 4.0F, new Dilation(0.0F)),
         ModelTransform.origin(16.0F, -5.0F, 18.0F));
 
-    ModelPartData leftfore = body.addChild("leftfore", ModelPartBuilder.create().uv(48, 47)
+    ModelPartData leftfore = body.addChild("leftfore", ModelPartBuilder.create().uv(49, 59)
             .cuboid(-2.0F, -2.0F, -2.0F, 14.0F, 4.0F, 4.0F, new Dilation(0.0F))
-            .uv(0, 72).cuboid(8.0F, 2.0F, -2.0F, 4.0F, 4.0F, 4.0F, new Dilation(0.0F)),
+            .uv(74, 77).cuboid(8.0F, 2.0F, -2.0F, 4.0F, 4.0F, 4.0F, new Dilation(0.0F)),
         ModelTransform.origin(16.0F, -5.0F, 10.0F));
 
-    ModelPartData rightfore = body.addChild("rightfore", ModelPartBuilder.create().uv(48, 55)
-            .cuboid(-12.0F, -2.0F, -8.0F, 14.0F, 4.0F, 4.0F, new Dilation(0.0F))
-            .uv(16, 72).cuboid(-12.0F, 2.0F, -8.0F, 4.0F, 4.0F, 4.0F, new Dilation(0.0F)),
-        ModelTransform.origin(-8.0F, -5.0F, 16.0F));
+    ModelPartData rightfore = body.addChild("rightfore", ModelPartBuilder.create().uv(49, 68)
+            .cuboid(-12.0F, -2.0F, -2.0F, 14.0F, 4.0F, 4.0F, new Dilation(0.0F))
+            .uv(81, 0).cuboid(-12.0F, 2.0F, -2.0F, 4.0F, 4.0F, 4.0F, new Dilation(0.0F)),
+        ModelTransform.origin(-8.0F, -5.0F, 10.0F));
+
+    ModelPartData leftpincer = body.addChild("leftpincer", ModelPartBuilder.create().uv(49, 26)
+            .cuboid(-4.0F, -1.0F, -16.0F, 8.0F, 8.0F, 16.0F, new Dilation(0.0F)),
+        ModelTransform.origin(12.0F, -6.0F, 4.0F));
+
+    ModelPartData rightpincer = body.addChild("rightpincer", ModelPartBuilder.create().uv(0, 26)
+            .cuboid(-4.0F, -2.0F, -16.0F, 8.0F, 8.0F, 16.0F, new Dilation(0.0F)),
+        ModelTransform.origin(-4.0F, -5.0F, 4.0F));
     return TexturedModelData.of(modelData, 128, 128);
   }
 
